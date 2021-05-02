d39b6b2e9d5705b12a833805ace2cc929c9fb1cb
jeongha-joo <jha970107@gmail.com>
2021-05-02 22:11:44 +0900

    secure mongoDB

---
+++ Readme.md
@@ -0,0 +1,11 @@
+npm init
+-> package.json 설치 및 npm 환경 세팅
+
+npm express --save
+-> spring boot처럼 쓰이는 node 프레임워크
+
+npm install body-parser --save
+-> json 처리하기 위한 도구?
+
+npm install nodemon --save-dev
+-> 서버 껐다 키지 않아도 코드 변화 감지해서 자동으로 업데이트해주는 친구
--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://Joo:abcd1234@boilerplate.rll8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hello worrrld"));
+app.get("/", (req, res) => res.send("hello worrrld kkk"));
+
+// 회원가입을 위한 route
+app.post("/register", (req, res) => {
+  // 회원 가입 할 떄 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  // mongodb에서 오는 메소드
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(port));
